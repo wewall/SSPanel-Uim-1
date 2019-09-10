ade3f7a8957d719cd2e44ecc8c81444cfa5e240e
围墙 <33481440+wewall@users.noreply.github.com>
2019-09-10 23:57:09 +0800

    Update Codepay.php

--- app/Services/Gateway/Codepay.php
+++ app/Services/Gateway/Codepay.php
@@ -95,34 +95,23 @@ class Codepay extends AbstractPayment
 
     public function notify($request, $response, $args)
     {
-        //以下五行无需更改
         ksort($_POST); //排序post参数
         reset($_POST); //内部指针指向数组中的第一个元素
         $codepay_key = Config::get('codepay_key'); //这是您的密钥
         $sign = '';//初始化
         foreach ($_POST as $key => $val) { //遍历POST参数
-            if ($val == '' || $key == 'sign') {
-                continue;
-            } //跳过这些不签名
-            if ($sign) {
-                $sign .= '&';
-            } //第一个字符串签名不加& 其他加&连接起来参数
+            if ($val == '' || $key == 'sign') continue; //跳过这些不签名
+            if ($sign) $sign .= '&'; //第一个字符串签名不加& 其他加&连接起来参数
             $sign .= "$key=$val"; //拼接为url参数形式
         }
         if (!$_POST['pay_no'] || md5($sign . $codepay_key) != $_POST['sign']) { //不合法的数据
-            exit('fail'); //返回失败，等待下次回调
+            exit('fail');  //返回失败 等待下次回调
+        } else { //合法的数据
+            //业务处理
+            $pay_id = $_POST['pay_id']; //需要充值的ID 或订单号 或用户名
+            $this->postPayment($pay_id, '码支付');
+            exit('success'); //返回成功 不要删除哦
         }
-
-//合法的数据
-        //业务处理
-        $pay_id = $_POST['pay_id']; //需要充值的ID 或订单号 或用户名
-        $money = (float)$_POST['money']; //实际付款金额
-        $price = (float)$_POST['price']; //订单的原价
-        //$param = $_POST['param']; //自定义参数
-        $pay_no = $_POST['pay_no']; //流水号
-        $this->postPayment($pay_id, '码支付');
-
-        exit('success'); //返回成功 不要删除哦
     }
 
 
